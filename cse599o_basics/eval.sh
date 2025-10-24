6672cbdc6d280912009d2ffd7d725115925dc643
Yuhan Ruan <andyubryh@gmail.com>
2025-10-24 04:05:44 -0700

    scripts
    Signed-off-by: Yuhan Ruan <andyubryh@gmail.com>

--- cse599o_basics/eval.sh
+++ cse599o_basics/eval.sh
@@ -1,3 +1,3 @@
 PYTHONUNBUFFERED=1 uv run eval.py \
-    --checkpoint_path ../ckpt/10/resume_warmup_500_cosine_5000_lr_1.5e-3_5e-5/ckpt5000.pt \
+    --checkpoint_path ../ckpt/best.pt \
     --val_data ../data/TinyStoriesV2-GPT4-valid.txt
--- cse599o_basics/predict.sh
+++ cse599o_basics/predict.sh
@@ -3,7 +3,7 @@ PROMPT="Once upon a time"
 
 export PYTHONUNBUFFERED=1
 max_tokens=512
-temperature_p_list="1_1 1_0.9 1_0.8 1_0.01 1000_1 0.8_1 0.01_1"
+temperature_p_list="1_1 1_0.9 1_0.8 1_0.01 1000_1 0.8_1 0.01_1 0.9_0.8"
 
 LOG_DIR=../logs/predict
 mkdir -p $LOG_DIR
